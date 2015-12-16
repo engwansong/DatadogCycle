0d343b6a199839559f4c9f053bb424f6b2c719f2
thomato <wansong@gmail.com>
2015-12-16 01:05:40 -0500

    Force play state to keep graph up to date

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 ## Changelog
 
+### 1.0.3
+* Force play state to keep graph up to date
+
 ### 1.0.2
 * Fix refresh cycle too short for fullscreen update
 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
     var graphs = [].slice.call(document.querySelectorAll('.fullscreen_trigger')),
       currentGraph = 0,
       selected = graphs.slice(),
-      interval;
+      interval
 
     /*
      * Create dom elements
@@ -18,17 +18,17 @@
         cancelable: true,
         view: window
       }),
-      closeButton = document.getElementById('cboxClose');
+      closeButton = document.getElementById('cboxClose')
 
-    prevButton.textContent = '<';
-    nextButton.textContent = '>';
-    startButton.textContent = 'Start';
-    stopButton.textContent = 'Stop';
+    prevButton.textContent = '<'
+    nextButton.textContent = '>'
+    startButton.textContent = 'Start'
+    stopButton.textContent = 'Stop'
 
-    el.appendChild(prevButton);
-    el.appendChild(nextButton);
-    el.appendChild(startButton);
-    el.appendChild(stopButton);
+    el.appendChild(prevButton)
+    el.appendChild(nextButton)
+    el.appendChild(startButton)
+    el.appendChild(stopButton)
 
     /*
      * Position widget
@@ -36,24 +36,24 @@
      */
     function position(event) {
       // Styles
-      el.style.position = 'fixed';
-      el.style['z-index'] = '10000';
+      el.style.position = 'fixed'
+      el.style['z-index'] = '10000'
       if (event) {
         var superbar = document.getElementById('superbar')
         if (superbar) {
           if (superbar.offsetTop === 0 && window.scrollY === 0) {
-            el.style.top = '64px';
+            el.style.top = '64px'
           } else if (window.scrollY < 42) {
-            el.style.top = (superbar.offsetTop - window.scrollY + 6) + 'px';
+            el.style.top = (superbar.offsetTop - window.scrollY + 6) + 'px'
           } else {
-            el.style.top = '22px';
+            el.style.top = '22px'
           }
 
-          el.style.right = '75px';
+          el.style.right = '75px'
         }
       } else {
-        el.style.top = '22px';
-        el.style.right = '55px';
+        el.style.top = '22px'
+        el.style.right = '55px'
       }
     }
 
@@ -110,41 +110,52 @@
     /*
      * Navigation functions
      */
+    function keepUpdated() {
+      var playButton = document.querySelector('#cboxContent .timeline_playpause.playing')
+      var pauseButton = document.querySelector('#cboxContent .timeline_playpause.paused')
+      while(!playButton) {
+        pauseButton.dispatchEvent(click)
+        playButton = document.querySelector('#cboxContent .timeline_playpause.playing')
+      }
+    }
     function next() {
-      currentGraph = (currentGraph + 1) % selected.length;
-      selected[currentGraph].dispatchEvent(click);
+      currentGraph = (currentGraph + 1) % selected.length
+      selected[currentGraph].dispatchEvent(click)
       render()
-    };
+      keepUpdated()
+    }
 
     function prev() {
       if (currentGraph === 0) {
-        currentGraph = selected.length - 1;
+        currentGraph = selected.length - 1
       } else {
         currentGraph--
       }
-      selected[currentGraph].dispatchEvent(click);
+      selected[currentGraph].dispatchEvent(click)
       render()
-    };
+      keepUpdated()
+    }
 
     function start() {
       if (interval) {
         clearInterval(interval)
       }
-      selected[currentGraph].dispatchEvent(click);
+      selected[currentGraph].dispatchEvent(click)
       interval = setInterval(function() {
-        next();
-      }, 20000);
+        next()
+      }, 15000)
       startButton.disabled = true
-      stopButton.disabled = false;
+      stopButton.disabled = false
       render()
-    };
+      keepUpdated()
+    }
 
     function stop() {
-      clearInterval(interval);
+      clearInterval(interval)
       stopButton.disabled = true
-      startButton.disabled = false;
+      startButton.disabled = false
       render()
-    };
+    }
 
     function close(event) {
       stop()
@@ -152,24 +163,24 @@
     }
 
     function render() {
-      position();
-      bindCloseButton();
+      position()
+      bindCloseButton()
     }
 
     /*
      * Bind events
      */
-    prevButton.addEventListener('click', prev);
-    nextButton.addEventListener('click', next);
-    startButton.addEventListener('click', start);
-    stopButton.addEventListener('click', stop);
+    prevButton.addEventListener('click', prev)
+    nextButton.addEventListener('click', next)
+    startButton.addEventListener('click', start)
+    stopButton.addEventListener('click', stop)
 
-    window.addEventListener('scroll', position);
+    window.addEventListener('scroll', position)
 
     /*
      * Inject
      */
-    document.body.appendChild(el);
+    document.body.appendChild(el)
 
     start()
     setupEnableGraph()
@@ -179,7 +190,7 @@
    * Handle DOM ready
    */
   if (document.readyState !== 'loading') {
-    init();
+    init()
   } else {
     document.addEventListener('DOMContentLoaded', init)
   }
@@ -191,26 +202,26 @@
    */
   function replaceText(selector, text) {
     var nodes = document.querySelectorAll(selector)
-    var numbers;
+    var numbers
     // Random number generator
     if (text === '%random%' && nodes.length > 0 && window.crypto) {
-      numbers = new Uint32Array(nodes.length);
-      window.crypto.getRandomValues(numbers);
+      numbers = new Uint32Array(nodes.length)
+      window.crypto.getRandomValues(numbers)
     }
     [].slice.call(document.querySelectorAll(selector))
       .forEach(function(element, index) {
         if (numbers) {
-          text = numbers[index];
+          text = numbers[index]
         }
-        element.textContent = text;
-      });
+        element.textContent = text
+      })
   }
   /*
    * Anonymize for screenshots.
    */
   function anonymize() {
-    replaceText('span.scope', 'Some scope');
-    replaceText('span.value', '%random%');
+    replaceText('span.scope', 'Some scope')
+    replaceText('span.value', '%random%')
     replaceText('span.metric', 'Some metric')
     replaceText('span.average', '%random%')
     replaceText('.chart_title', 'Some Graph')
@@ -220,7 +231,7 @@
 
   window.addEventListener('keypress', function(e) {
     if (e.keyCode === 46) {
-      anonymize();
+      anonymize()
     }
   })
-})(window, document);
+})(window, document)
